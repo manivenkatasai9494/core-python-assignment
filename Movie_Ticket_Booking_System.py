d225c52c92a2613e0240d523e8d076031d5c0f2f
manivenkatasai <medammani4@gmail.com>
2024-12-19 23:25:52 +0530

    moviebooking basic template added

---
+++ Movie_Ticket_Booking_System.py
@@ -0,0 +1,19 @@
+class MovieTicketBookingSystem:
+    totalSeats = 10
+    bookedSeats = []
+
+    def ShowAvailableTickets(self):
+
+
+    def BookSeat(self):
+        print("Available Tickets seats are = ")
+
+    def CancelSeat(self):
+
+
+    def ShowBookedSeats(self):
+
+
+    def Close(self):
+
+
